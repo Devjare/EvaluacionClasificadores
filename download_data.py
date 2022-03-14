90744ead7264585a686520d9df761a8bc7adddba
Andres Reyna <andresreyna15@gmail.com>
2022-03-13 21:36:44 -0600

    First commit. Pending separating activities

---
+++ download_data.py
@@ -0,0 +1,14 @@
+# For zip extraction.
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
+
+url_dataset = "https://www.tamps.cinvestav.mx/~wgomez/material/RP/tarea1/datos4.zip"
+
+def download_and_unzip(url, extract_to='.'):
+    http_response = urlopen(url)
+    zipfile = ZipFile(BytesIO(http_response.read()))
+    zipfile.extractall(path=extract_to)
+
+
+download_and_unzip(url_dataset, "./data/")
